03bf5859401c0fb7d2892b6182356372c7548651
tomekaks <tomekaks@gmail.com>
2023-02-26 10:26:01 +0100

    Added UI project

---
+++ BigMuscleShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BigMuscleShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
